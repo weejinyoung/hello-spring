88afc93e6af1e96013cf7d12cca8c839dbb3024e
weejinyoung <a8118199@gmail.com>
2022-01-13 12:02:14 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,28 @@
+package hello.hellospring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
